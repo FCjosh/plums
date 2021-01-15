b9c012bd9b57220b4067918e0d620319a84471f7
FCjosh <joshua.rouault@gmail.com>
2021-01-14 20:33:03 -0700

    Filter and SearchOptions work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 
 initReactFastclick();
 
+const searchSelection = {
+  'Sort Direction': 'asc',
+  'Sort': 'meta-score',
+  'Intolerance': [],
+  'Cuisine': [],
+  'Diet': [],
+  'Meal Types': [],
+}
+
 function App() {
   const [searchValue, setSearchValue] = useState('');
   const [error, setError] = useState(null);
@@ -12,20 +21,20 @@ function App() {
   const [showing, setShowing] = useState('home');
   const [isLoad, setIsLoad] = useState(false);
   const [recipe, setRecipe] = useState({});
-  const [sort, setSort] = useState("meta-score");
-  const [sortDirection, setSortDirection] = useState('asc');
   const [showSort, setShowSort] = useState(false);
+  const [showFilter, setShowFilter] = useState(false);
+  const [showFilterOption, setShowFilterOption] = useState('none');
+  const [searchSelectionState, setSearchSelectionState] = useState({
+    'Sort Direction': 'asc',
+    'Sort': 'meta-score',
+    'Intolerance': [],
+    'Cuisine': [],
+    'Diet': [],
+    'Meal Types': [],
+  });
 
-  const searchAPI = (sortp, sortDirectionp) => {
-    let newsort = sort;
-    if(sortp){
-      newsort = sortp;
-    }
-    let newsortDirection = sortDirection;
-    if(sortDirectionp){
-      newsortDirection = sortDirectionp;
-    }
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchValue}&number=10&sort=${newsort}&sortDirection=${newsortDirection}&apiKey=cacaef5c287c4b159cf6aeb1dc609470`)
+  const searchAPI = () => {
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${searchValue}&number=9&sort=${searchSelection.Sort}&sortDirection=${searchSelection['Sort Direction']}&cuisine=${searchSelection.Cuisine}&diet=${searchSelection.Diet}&intolerances=${searchSelection.Intolerance}&type=${searchSelection.['Meal Types']}&apiKey=cacaef5c287c4b159cf6aeb1dc609470`)
     .then(res => res.json())
     .then(
       (result) => {
@@ -66,23 +75,34 @@ function App() {
     setShowing('search');
     searchAPI();
   }
-  const handleSort = (sortp, sortDirectionp) => {
+  const handleSearchOptions = () => {
     if(showing === "search"){
-      searchAPI(sortp, sortDirectionp);
+      searchAPI();
     }
   }
-
-  const sortTypes = [
-    'meta-score',
-    'popularity',
-    'healthiness',
-    'price',
-    'time',
-    'random',
-    'calories',
-    'carbohydrates',
-    'carbs',
-  ]
+  const searchData = {
+    'Sort': [
+      'meta-score', 'popularity', 'healthiness', 'price', 'time', 'random', 'calories', 'carbohydrates', 'carbs',
+    ],
+    'Filter': {
+      'Intolerance': [
+        'Dairy', 'Egg', 'Gluten', 'Grain', 'Peanut', 'Seafood', 'Sesame', 'Shellfish', 'Soy', 'Sulfite', 'Tree Nut', 'Wheat'
+      ],
+      'Cuisine': [
+        'African', 'American', 'British', 'Cajun', 'Caribbean', 'Chinese',
+        'Eastern European', 'European', 'French', 'German', 'Greek', 'Indian', 'Irish', 'Italian', 'Japanese', 'Jewish', 'Korean',
+        'Latin American', 'Mediterranean', 'Mexican', 'Middle Eastern', 'Nordic', 'Southern', 'Spanish', 'Thai', 'Vietnamese',
+      ],
+      'Diet': [
+        'Gluten Free',
+        'Ketogenic', 'Vegetarian', 'Lacto-Vegetarian', 'Ovo-Vegetarian', 'Vegan', 'Pescetarian', 'Paleo', 'Primal', 'Whole30',
+      ],
+      'Meal Types': [
+        'main course', 'side dish', 'dessert', 'appetizer',
+        'salad', 'bread', 'breakfast', 'soup', 'beverage', 'sauce', 'marinade', 'fingerfood', 'snack', 'drink',
+      ],
+    },
+  }
 
   return (
     <div className="App">
@@ -101,27 +121,77 @@ function App() {
                     } else {
                       setShowSort(true);
                     }
-                  }}><p>Sort: {sort}</p></button>
-                  <button className="sortDir" onClick={() => {
-                    let sortDirectionp = "asc";
-                    if(sortDirection === "asc"){
-                      setSortDirection("desc");
-                      sortDirectionp ="desc";
-                    }else{
-                      setSortDirection("asc");
+                    if(showFilter){
+                      setShowFilter(false);
                     }
-                    handleSort("",sortDirectionp);
-                  }} >{sortDirection === "asc" ? <p>&#9650;</p> : <p>&#9660;</p>}</button>
+                    if(showFilterOption !== 'none'){
+                      setShowFilterOption('none');
+                    }
+                  }}><p>Sort: {searchSelectionState.Sort}</p></button>
+                  <button className="sortDir" onClick={() => {
+                    if(searchSelection['Sort Direction'] === 'asc'){
+                      searchSelection['Sort Direction'] = 'desc'
+                    }else{
+                      searchSelection['Sort Direction'] = 'asc'
+                    }
+                    setSearchSelectionState(JSON.parse(JSON.stringify(searchSelection)));
+                    handleSearchOptions();
+                  }} >{searchSelection['Sort Direction'] === "asc" ? <p>&#9650;</p> : <p>&#9660;</p>}</button>
+                  <button className={ showFilter ? "dropbtnd" : "dropbtn"} onClick={() => {
+                    if(showFilter){
+                      setShowFilter(false);
+                      handleSearchOptions();
+                    } else {
+                      setShowFilter(true);
+                    }
+                    if(showFilterOption !== 'none'){
+                      setShowFilterOption('none');
+                    }
+                    if(showSort){
+                      setShowSort(false);
+                    }
+                  }}><p>Filter</p></button>
                 </div>
               </div>
               { showSort ?
                 <div className="dropdown-content">
-                  {sortTypes.map(item => (
+                  {searchData['Sort'].map(item => (
                     <button onClick={() => {
-                      setSort(item);
-                      handleSort(item, "");
+                      searchSelection.Sort = item;
+                      setSearchSelectionState(JSON.parse(JSON.stringify(searchSelection)));
+                      handleSearchOptions();
                       setShowSort(false);
-                    }} className="dropdown-item" key={item}>{item}</button>
+                    }} className="dropdown-item" key={item} style={{ backgroundColor: searchSelectionState.Sort !== item ?'#BB86FC' : '#3700B3' }}>{item}</button>
+                  ))}
+                </div>
+                : <div></div>
+              }
+              { showFilter ?
+                <div className="dropdown-content">
+                  {Object.keys(searchData.Filter).map(key => (
+                    <button className={"dropbtn"} onClick={() => {
+                      if(showFilterOption !== key){
+                        setShowFilterOption(key);
+                      } else {
+                        setShowFilterOption('none');
+                      }
+                    }} key={key} style={{ backgroundColor: showFilterOption !== key ?'#BB86FC' : '#3700B3' }}><p>{key}: {searchSelectionState[key].length < 4 ? searchSelectionState[key].join(', ') : searchSelectionState[key][0] + ' ...'}</p></button>
+                  ))}
+                </div>
+                : <div></div>
+              }
+              { showFilterOption !== 'none' ?
+                <div className="dropdown-filter-content">
+                  {searchData.Filter[showFilterOption].map(key => (
+                    <button onClick={() => {
+                      if(searchSelection[showFilterOption].includes(key)){
+                        const index = searchSelection[showFilterOption].indexOf(key);
+                        searchSelection[showFilterOption].splice(index, 1);
+                      } else {
+                        searchSelection[showFilterOption].push(key);
+                      }
+                      setSearchSelectionState(JSON.parse(JSON.stringify(searchSelection)));
+                    }} className="dropdown-item" key={key} style={{ backgroundColor: !searchSelection[showFilterOption].includes(key) ?'#BB86FC' : '#3700B3' }}>{key}</button>
                   ))}
                 </div>
                 : <div></div>
